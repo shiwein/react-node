1cf4e39f54bcf85941bf34bcb0e801696d0e930e
Weinan Shi <shi.wein@northeastern.edu>
2023-12-04 00:58:27 -0800

    a6

--- app.js
+++ app.js
@@ -2,20 +2,43 @@ import express from 'express';
 import cors from 'cors';  // Import cors using ES6 syntax
 import Hello from './hello.js';
 import Lab5 from './lab5.js';
+import UserRoutes from "./users/routes.js";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import AssignmentsRoutes from './assignments/routes.js';
 import "dotenv/config";
+import mongoose from "mongoose";
+import session from "express-session";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    })
+);   
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
+);
 app.use(express.json());
-
 Hello(app);
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
 Lab5(app);
-
 app.listen(process.env.PORT || 4000);
