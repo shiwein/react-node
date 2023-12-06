3f1301c681c38bb1ae479d6ada851468884731f6
Weinan Shi <shi.wein@northeastern.edu>
2023-12-06 15:02:42 -0800

    ?

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-import express from 'express';
-import cors from 'cors';  // Import cors using ES6 syntax
-import Hello from './hello.js';
-import Lab5 from './lab5.js';
+// const express = require("express");
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
 import UserRoutes from "./users/routes.js";
-import CourseRoutes from './courses/routes.js';
-import ModuleRoutes from './modules/routes.js';
-import AssignmentsRoutes from './assignments/routes.js';
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentsRoutes from "./assignments/routes.js";
 import "dotenv/config";
 import session from "express-session";
 import mongoose from "mongoose";
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(
     cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
     })
-);   
+);
+
 const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-sessionOptions.proxy = true;
-sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
     };
 }
-app.use(
-    session(sessionOptions)
-);
+app.use(session(sessionOptions));
+
 app.use(express.json());
 Hello(app);
 UserRoutes(app);
